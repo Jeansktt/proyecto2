08c7c36f1db6e07cdd75c00dbbcfb62566ef38b0
marcsalou23 <127138098+marcsalou23@users.noreply.github.com>
2023-08-10 15:44:17 +0200

    Cambio en rutas

--- controllers/notes/getNotes.js
+++ controllers/notes/getNotes.js
@@ -1,21 +1,21 @@
 const selectAllNotesIdQuery = require('../../db/queries/notes/selectAllNotesIdQuery');
 
 const getNote = async (req, res, next) => {
-  try {
-    const { noteId } = req.params;
-    // Para ver las entradas hay que estar logueados.
-    const notes = await selectAllNotesIdQuery(noteId, req.user.id);
+    try {
+        const { noteId } = req.params;
+        // Para ver las entradas hay que estar logueados.
+        const note = await selectAllNotesIdQuery(noteId, req.user.id);
 
-    // Enviar la respuesta con la nota obtenida
-    res.send({
-      status: 'Success',
-      data: {
-        notes,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
+        // Enviar la respuesta con la nota obtenida
+        res.send({
+            status: 'Success',
+            data: {
+                note,
+            },
+        });
+    } catch (err) {
+        next(err);
+    }
 };
 
 module.exports = getNote;
